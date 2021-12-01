9cef49dd7725db7f8866f0056cf4ea35665d661c
LuukVM <43540296+LuukVM@users.noreply.github.com>
2021-12-01 12:11:06 +0000

    Add files via upload

---
+++ Day 1/part1.py
@@ -0,0 +1,16 @@
+file = open("values.txt", "r")
+
+values = file.read().splitlines()
+
+previousValue = 0
+increases = 0
+
+
+for x in range(len(values)):
+    if int(values[x]) > previousValue and x != 0:
+        increases = increases + 1
+    previousValue = int(values[x])
+
+print(increases)
+
+file.close()
